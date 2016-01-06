05a85f2e99df79113dc1913a2701ba38a8dafec5
abonte <bonte@outlook.it>
2016-01-06 18:26:58 +0100

    add resources

---
+++ src/introsde/finalproject/rest/ss/resources/PersonResource.java
@@ -0,0 +1,69 @@
+package introsde.finalproject.rest.ss.resources;
+
+//import ehealth.model.MeasureDefinition;
+import introsde.finalproject.soap.ws.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Stateless // only used if the the application is deployed in a Java EE container
+@LocalBean // only used if the the application is deployed in a Java EE container
+public class PersonResource {
+    @Context
+    UriInfo uriInfo;
+    @Context
+    Request request;
+    int id;
+    private People people = null;
+
+    EntityManager entityManager; // only used if the application is deployed in a Java EE container
+
+    public PersonResource(UriInfo uriInfo, Request request,int id, EntityManager em) {
+        this.uriInfo = uriInfo;
+        this.request = request;
+        this.id = id;
+        this.entityManager = em;
+    }
+
+    public PersonResource(UriInfo uriInfo, Request request,int id, People people) {
+        this.uriInfo = uriInfo;
+        this.request = request;
+        this.id = id;
+        this.people = people;
+    }
+
+    // Application integration
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response getPerson() {
+    	System.out.println("////sono in get person");
+        Person person = people.getPerson(id);
+       if (person == null)
+           return Response.status(Response.Status.NOT_FOUND)
+        		   .entity("Get: Person with " + id + " not found").build();
+       else
+          return Response.ok(person).build();
+    }
+    
+}
